17ca3dcf91aa6e0f1a1aa481ce2dfde6df8d098c
lorenaneiva <lorenaneiva2232820@gmail.com>
2026-03-18 17:59:47 -0300

    navbar - initial style

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="">link</a></li>
+      <li><a href="">link</a></li>
+      <li><a href="">link</a></li>
+      <li><a href="">link</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+a{
+  text-decoration: none;
+  color: #2c3e50;
+}
+ul{
+  display: flex;
+  list-style: none;
+  justify-content: end;
+}
+li{
+  padding: 20px;
+  margin: 10px;
+}
+</style>
